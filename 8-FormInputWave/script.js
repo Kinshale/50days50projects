dd7c4c3ea83d84a7a6ad27b8b4fd69e735c23848
Kinshale <kinshale.world@yahoo.com>
2022-09-23 16:46:54 +0200

    Added waveAnim with CSS

--- 8-FormInputWave/script.js
+++ 8-FormInputWave/script.js
@@ -5,14 +5,23 @@ let [emailInput, passwordInput] = document.querySelectorAll(
   ".form-control input"
 );
 
-let button = document.querySelector("button");
-
 handleAddSpanElem(emailLabel);
 handleAddSpanElem(passwordLabel);
 
 function handleAddSpanElem(string) {
   string.innerHTML = string.innerText
     .split("")
-    .map((letter) => `<span class = "not-selected">${letter}</span>`)
+    .map(
+      (letter, index) =>
+        `<span style = "transition-delay: ${index * 100}ms">${letter}</span>`
+    )
     .join("");
+}
+
+let button = document.querySelector("button");
+
+button.addEventListener("mousedown", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
 }
